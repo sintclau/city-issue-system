3e8ce1deb3957a1477053255bcdb5aa4620af7c3
Claudiu Spiescu <claudiu.spiescu@redevia.ro>
2026-04-15 11:42:47 +0300

    feat: implement report structure

--- city_manager.c
+++ city_manager.c
@@ -8,6 +8,19 @@
 
 char *program_name = NULL;
 typedef enum { ROLE_NONE, ROLE_INSPECTOR, ROLE_MANAGER } role_t;
+typedef struct {
+    double latitude;
+    double longitude;
+}gps_coordinates_t;
+typedef enum { ISSUE_NONE, ISSUE_ROADS, ISSUE_LIGHTING, ISSUE_FLOODING } issue_category_t;
+typedef struct {
+    int id;
+    char inspector[50];
+    gps_coordinates_t location;
+    issue_category_t category;
+    int severity;
+    char description[256];
+}
 
 static void print_usage(const char *prog) {
     printf("Usage: %s --role <inspector|manager> <action> [args...]\n", prog);
